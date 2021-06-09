0edb3afa844945866cdbdbfddc2a5a7bb6dee4b3
equesada <equesada@ludonkey.com>
2021-06-09 12:06:06 +0200

    BugFix: Problem with the migrations files

--- migrations/Version20210609095719.php
+++ migrations/Version20210609095719.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210607140301 extends AbstractMigration
+final class Version20210609095719 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,16 @@ final class Version20210607140301 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE destination (id INT AUTO_INCREMENT NOT NULL, author_id INT DEFAULT NULL, city VARCHAR(255) NOT NULL, country VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, img_url VARCHAR(255) NOT NULL, INDEX IDX_3EC63EAAF675F31B (author_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE destination ADD CONSTRAINT FK_3EC63EAAF675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE destination DROP FOREIGN KEY FK_3EC63EAAF675F31B');
+        $this->addSql('DROP TABLE destination');
         $this->addSql('DROP TABLE user');
     }
 }
